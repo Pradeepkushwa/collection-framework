9bdf071c72045b2aca91f6334e4584413e7ee5f6
pradeep <pradeepkushwaha232365@gmail.com>
2023-02-14 23:50:55 +0530

    first commit

---
+++ collectionclass1.java
@@ -0,0 +1,11 @@
+package collectionframework;
+
+import java.util.ArrayList;
+
+public class collectionclass1 {
+    public static void main(String[] args){
+        ArrayList al=new ArrayList();
+        al.add(12);
+        al.add(13);
+    }
+}
